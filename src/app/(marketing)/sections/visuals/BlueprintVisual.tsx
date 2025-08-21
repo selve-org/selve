09c24b8de23f84f10cd797c1c858970965855f0e
chrisedeson <edesonchristopher@gmail.com>
2025-08-21 01:26:47 +0100

    Add fingerprint_invert image and update BlueprintVisual styles for light mode

--- src/app/(marketing)/sections/visuals/BlueprintVisual.tsx
+++ src/app/(marketing)/sections/visuals/BlueprintVisual.tsx
@@ -3,15 +3,11 @@ import React from "react";
 
 const BlueprintVisual: React.FC = () => (
   <div
-    className="w-full h-72 flex items-center justify-center"
-    style={{
-      backgroundImage: "url(/images/fingerprint_2.0.jpg)",
-      backgroundSize: "contain",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-      pointerEvents: "none",
-      userSelect: "none",
-    }}
+    className={`
+      w-full h-72 flex items-center justify-center
+      bg-center bg-no-repeat bg-contain pointer-events-none select-none
+      bg-[url('/images/fingerprint_invert.png')] dark:bg-[url('/images/fingerprint_2.0.jpg')]
+    `}
   />
 );
 
